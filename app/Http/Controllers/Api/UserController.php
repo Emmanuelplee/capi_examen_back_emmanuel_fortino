847a84bee2e42569f6609ef3ae6ef71b820c86b5
emmanuel <emmanuelplee@gmail.com>
2023-04-27 22:04:35 -0600

    add: UserController y route api/index

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Carbon\Carbon;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $users = User::with('userDomicilio')->get();
+        $data = array();
+        if (count($users) > 0) {
+            $data = $users->map(function ($user) {
+                $age = Carbon::parse($user->fecha_nacimiento)->age;
+                return [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'edad' => $age,
+                    'user_domicilio' => (object)[
+                        'domicilio' => $user->userDomicilio->domicilio,
+                        'numero_exterior' => $user->userDomicilio->numero_exterior,
+                        'colonia' => $user->userDomicilio->colonia,
+                        'cp' => $user->userDomicilio->cp,
+                        'ciudad' => $user->userDomicilio->ciudad,
+                    ]
+                ];
+            });
+        }
+        return response()->json(['ok'=> true, 'data' => $data]);
+    }
+}
